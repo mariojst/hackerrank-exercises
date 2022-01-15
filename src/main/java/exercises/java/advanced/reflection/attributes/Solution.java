de2886327bec76eb78ee6d475833eacdfe13337d
mariojst@hotmail.com <Mabu1608!@>
2022-01-15 22:14:23 +0000

    Submit Java Reflection Attributes problem and solution

---
+++ src/main/java/exercises/java/advanced/reflection/attributes/Solution.java
@@ -0,0 +1,21 @@
+package exercises.java.advanced.reflection.attributes;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+    public static void main(String[] args){
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method method : methods){
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+}
